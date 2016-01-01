7ab90605ebddb4e1cd1aa0ac604f04465cb18367
Mahmood Hanif <mmhanif@gmail.com>
2015-12-31 23:46:12 -0500

    Init commit of Python based client

---
+++ golduino.py
@@ -0,0 +1,63 @@
+import serial
+
+SeedChoices = ['Input_Seed', 'R_Pentamino', 'Acorn', 'Diehard', 'Pattern_10', 'Pattern_5x5', 'Random_Seed', 'Random_Choice']
+SeedChoiceDict = dict(zip(SeedChoices, range(len(SeedChoices))))
+
+ColorChoices = ['Input_Color', 'Red', 'Green', 'Blue', 'Random_Color', 'Continuous_Random']
+ColorChoiceDict = dict(zip(ColorChoices, range(len(ColorChoices))))
+
+
+_ser = serial.Serial()
+_mode = ''
+
+def init(port, baudrate):
+    _ser.port = port
+    _ser.baudrate = baudrate
+    _ser.open()
+
+def configure():
+    if _mode != 'Configure':
+        _ser.write('c')
+        _mode = 'Configure'
+
+def step():
+    if _mode != 'Running_Step':
+        _ser.write('s')
+        _mode = 'Running_Step'
+
+def run():
+    if _mode != 'Running_Continuous':
+        _ser.write('t')
+        _mode = 'Running_Continuous'
+
+def reset():
+    _ser.write('r')
+    // _mode stays whatever it was before
+
+# If seed_choice == 'Input_Seed',
+# points should contain a list of two-integer tuples
+def configure_seed(seed_choice, points=[]):
+    if _mode == 'Configure':
+        if seed_choice in SeedChoices:
+            _ser.write('e%d' % SeedChoiceDict[seed_choice])
+            if seed_choice == 'Input_Seed':
+                for pt in points:
+                    _ser.write(',%d,%d' % pt)
+                _ser.write(',-1')
+
+# If color_choice == 'Input_Color',
+# rgb should be a three-integer tuple, with each integer in the range 0-255
+def configure_color(color_choice, rgb=None):
+    if _mode == 'Configure':
+        if color_choice in ColorChoices:
+            _ser.write('e%d' % SeedChoiceDict[color_choice])
+            if seed_choice == 'Input_Color':
+                _ser.write(',%d,%d,%d' % rgb)
+
+def configure_iteration_interval(interval):
+    if _mode == 'Configure':
+        _ser.write('i%d' % interval)
+
+def step_next():
+    if _mode == 'Running_Step':
+        _ser.write('n')
